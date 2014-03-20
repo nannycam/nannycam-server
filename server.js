2ffcbf3a3628c1178c8703e2e7a830da725ae41b
Aaron Davis <magwe25@gmail.com>
2014-03-19 21:33:38 -0400

    added loop to random point test

--- server.js
+++ server.js
@@ -7,11 +7,18 @@ var server = net.createServer(function (sock) {
 	sock.on('end', function() {
     	console.log('server disconnected');
   	});
-	var points = {"points":[{"x":3,"y":4}, {"x":12,"y":14}, {"x":45,"y":1}, {"x":5,"y":6}, {"x":9,"y":5}, {"x":2,"y":2}]};
-	sock.write(JSON.stringify(points));
-	sock.pipe(sock);
+  	setInterval(function(){
+  		var points = {"points":[{"x":getRandomPoint(),"y":getRandomPoint()}]};
+		sock.write(JSON.stringify(points)+"\r\n");
+  	},1500);
+  	sock.pipe(sock);
+
 });
 
 server.listen(8080, function () {
 	console.log("Server started on port 8080");
 });
+
+function getRandomPoint () {
+    return Math.round(Math.random() * (100 - 1) + 1);
+}
