c80e98f75daec3882257ba8877443bd9faee994d
David Paul <dapaul@cs.unc.edu>
2014-03-19 21:02:41 -0400

    Update server_test.js

--- tests/server_test.js
+++ tests/server_test.js
@@ -1,5 +1,4 @@
 
-
 var net = require("net");
 
 // create client
@@ -8,3 +7,13 @@ var net = require("net");
 
 // ping the server for data
 // or output what the server sends
+
+var sys = require("sys"),  
+my_http = require("http");  
+my_http.createServer(function(request,response){  
+    sys.puts("I got kicked");  
+    response.writeHeader(200, {"Content-Type": "text/plain"});  
+    response.write("Hello World");  
+    response.end();  
+}).listen(8080);  
+sys.puts("Server Running on 8080");   
