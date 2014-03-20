134b2ce41bb81b7c799b59f57964428535059deb
David Paul <dapaul@cs.unc.edu>
2014-03-19 22:03:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
-
-
 var net = require("net");
 
+var server = net.createServer(function (sock) {
+console.log("Server connected");
+sock.on('end', function() {
+     console.log('server disconnected');
+     clearInterval(testloop);
+   });
+   var testloop = setInterval(function(){
+   var points = {"points":[{"x":getRandomPoint(),"y":getRandomPoint()}]};
+sock.write(JSON.stringify(points)+"\r\n");
+console.log("sent points : " + JSON.stringify(points));
+   },1500);
+   sock.pipe(sock);
 
-var server = net.createServer(function (c) {
-	console.log("Server connected");
 });
 
 server.listen(8080, function () {
-	console.log("Server started on port 8080");
+console.log("Server started on port 8080");
 });
+
+function getRandomPoint () {
+    return Math.round(Math.random() * (100 - 1) + 1);
+}
