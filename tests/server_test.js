558896d182ccf42b51d5e54b7e9ea4f1c3140afa
Kai Lieth <klieth@live.unc.edu>
2014-03-19 21:22:27 -0400

    Revert "Update server_test.js"
    This reverts commit c80e98f75daec3882257ba8877443bd9faee994d.

--- tests/server_test.js
+++ tests/server_test.js
@@ -1,4 +1,5 @@
 
+
 var net = require("net");
 
 // create client
@@ -7,13 +8,3 @@ var net = require("net");
 
 // ping the server for data
 // or output what the server sends
-
-var sys = require("sys"),  
-my_http = require("http");  
-my_http.createServer(function(request,response){  
-    sys.puts("I got kicked");  
-    response.writeHeader(200, {"Content-Type": "text/plain"});  
-    response.write("Hello World");  
-    response.end();  
-}).listen(8080);  
-sys.puts("Server Running on 8080");   
