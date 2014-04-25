7c5252bb5606d6d0e13ad28c3a1072a1f7d2a266
Kai Lieth <klieth@cs.unc.edu>
2014-04-25 05:22:43 +0000

    Made some changes to demo sending a buffer

--- server.js
+++ server.js
@@ -4,20 +4,22 @@ var server = net.createServer(function (sock) {
 	console.log("Server connected");
 	sock.on('end', function() {
     	console.log('server disconnected');
-    	clearInterval(testloop);
   	});
-  	var testloop = setInterval(function(){
-  		var points = {"points":[{"x":getRandomPoint(),"y":getRandomPoint()}]};
-		sock.write(JSON.stringify(points)+"\r\n");
-		console.log("sent points : " + JSON.stringify(points));
-  	},1500);
-  	sock.pipe(sock);
+
+	var buf = new Buffer(640*480*3);
+	for (var i = 0; i < buf.length; i++) {
+		buf.writeUInt32LE(getRandomPoint(),true);
+		if (i % 100000 == 0) console.log(buf[i-1]);
+	}
+
+	sock.write(buf);
+	sock.end();
 });
 
 server.listen(8080, function () {
-console.log("Server started on port 8080");
+	console.log("Server started on port 8080");
 });
 
 function getRandomPoint () {
-    return Math.round(Math.random() * (100 - 1) + 1);
+    return Math.round(Math.random() * 255);
 }
