28d3beb9d1f62b653974d1b7f1929c160dc58608
Kai Lieth <klieth@live.unc.edu>
2014-04-25 01:12:36 -0400

    Added unit test

--- tests/server_test.js
+++ tests/server_test.js
@@ -1,17 +1,23 @@
 var net = require('net');
 
-var client = new net.Socket();
-client.connect(8080, '127.0.0.1', function() {
 
-    console.log('CONNECTED TO: ' + '127.0.0.1' + ':' + 8080);
-});
+exports['data format'] = function (test) {
+	test.expect(3);
+	var client = new net.Socket();
+	client.connect(8080, '127.0.0.1', function() {
+		console.log('CONNECTED TO: ' + '127.0.0.1' + ':' + 8080);
+	});
+	client.on('data', function(data) {
+		var d = JSON.parse(data);
+		console.dir(d);
+		test.ok(d);
+		test.ok(d.points);
+		test.equal(d.points.length,1);
+		client.destroy();
+	});
+	client.on('close', function() {
+		//console.log('Connection closed');
+		test.done();
+	});
+};
 
-client.on('data', function(data) {
-    
-    console.log('DATA: ' + data);
-
-});
-
-client.on('close', function() {
-    console.log('Connection closed');
-});
