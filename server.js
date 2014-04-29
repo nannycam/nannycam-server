88b5f915c105afc6f02c62d1f89fcf5a9d6b072c
Aaron Davis <magwe25@gmail.com>
2014-04-28 23:31:25 -0400

    added auth code I think

--- server.js
+++ server.js
@@ -1,18 +1,49 @@
 var net = require("net");
+var user = "bob";
+var theUser = "";
+var pw = "5f4dcc3b5aa765d61d8327deb882cf99";
+var thePw = "";
+var correctpw= "";
+var state = 0;
+var unamepatt = /^USER - |.*/
+var passpatt = /^PASS - |./
 
 var server = net.createServer(function (sock) {
 	console.log("Server connected");
 	sock.on('end', function() {
     	console.log('server disconnected');
+      close();
   	});
-
-	var buf = new Buffer(640*480*3);
-	for (var i = 0; i < buf.length; i++) {
-		buf.writeUInt32LE(getRandomPoint(),true);
-		if (i % 100000 == 0) console.log(buf[i-1]);
-	}
-
-	sock.write(buf);
+  sock.on('data',function(data) {if(state == 1){
+      if(unamepatt.test(data)){
+        theUser = data.substr(7);
+        //see if user exists
+        if(theUser.match(user)){
+          //get user's info from db
+          thePw = pw;
+          state++;
+        }
+      }
+    }
+    if(state == 2){
+      if(passpatt.test(data)){
+        thePw = data.substr(7);
+        if(thePw.match(pw)){
+          state++;
+        }
+      }
+    }
+    if(state == 3){
+      //start colors
+    }
+  });
+  sock.on('connect',function(data) {
+    if(state == 0){
+      sock.write("ok");
+      state++;
+    }
+    
+  });
 	sock.end();
 });
 
